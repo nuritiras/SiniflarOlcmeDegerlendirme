84c1c2291cdd6dc3f7fbf0ef19ada0a1c5309a49
Nuri TIRAŞ <47992544+nuritiras@users.noreply.github.com>
2023-01-06 00:11:22 +0300

    Onuncu Sorunun Çözümü Eklendi.

---
+++ OnuncuSoru/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnuncuSoru
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public sealed class Televizyon
+    {
+        //MÜHÜRLÜ SINIFLAR (SEALED CLASSES)
+        //Bir sınıftan bir başka sınıf türetilmek istenmediğinde bu
+        //sınıfı sealed anahtar kelimesiyle mühürlü tanımlamak
+        //gerekir.
+    }
+
+    public class AkilliTelevizyon : Televizyon // HATA !!!
+    {
+        
+    }
+}
